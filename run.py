bd8ebdd02d5cc484b983b2d9e5510e467c9be5f0
Lowyihong <lowyihong1314@gmail.com>
2026-03-22 10:08:00 +0000

    fixing vite output send file bug

--- run.py
+++ run.py
@@ -1,11 +1,18 @@
-import eventlet
+def _build_app():
+    if __name__ == "__main__":
+        import eventlet
 
-eventlet.monkey_patch()
+        eventlet.monkey_patch()
 
-from app import create_app, socketio
-from _token import DEBUG, DEBUG_PORT
+    from app import create_app, socketio
 
-app = create_app(socket=True)
+    return create_app(socket=True), socketio
+
+
+app, socketio = _build_app()
+
 
 if __name__ == "__main__":
+    from _token import DEBUG, DEBUG_PORT
+
     socketio.run(app, host="0.0.0.0", port=DEBUG_PORT, debug=DEBUG)
